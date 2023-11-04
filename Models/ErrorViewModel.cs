df3a691c92ae0510f6d444e08b33d3530522e21b
joaogabriel87 <joagabriel9@gmail.com>
2023-11-03 21:59:27 -0300

    Criação projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Check_in_List.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
